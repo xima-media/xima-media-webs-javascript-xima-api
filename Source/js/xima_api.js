c959245b3821f75c49db6ac522b581333d63da70
Sebastian Gierth <sgi@xima.de>
2015-12-09 13:54:20 +0100

    [Add] new main object

---
+++ Source/js/xima_api.js
@@ -0,0 +1,15 @@
+/*!
+ * XIMA Javascript API
+ *
+ * @author XIMA MEDIA GmbH
+ * @licence MIT
+ * @version 2.0.0
+ */
+var XIMA = {
+    api: {
+    }
+};
+/**
+ * Register modules here ...
+ * e.g. XIMA.api.module = moduleObject;
+ */
